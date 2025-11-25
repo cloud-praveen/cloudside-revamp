594cc1ffd8ed0209a03921d3bf6df2a908444b7f
Your Name <you@example.com>
2025-11-25 11:42:40 +0530

    initialize React project with Vite and Tailwind v4 configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <h1 className="text-4xl font-bold text-green-600">Hello developer Are You Ready 🚀</h1>
+    </>
+  )
+}
+
+export default App
